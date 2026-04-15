de795ef649d834f48ad2f8d4c4d68274f649b0cb
karma <275169041+pastsimpledev@users.noreply.github.com>
2026-04-15 09:03:55 +0200

    Update bomba.js

--- plugins/bomba.js
+++ plugins/bomba.js
@@ -123,7 +123,7 @@ async function esplosione(chatId, conn, m) {
     await conn.sendMessage(chatId, { 
         text: finale, 
         mentions: [b.vittima, ...b.passaggi],
-        footer: '𝖇𝖑𝖔𝖔𝖉𝖇𝖔𝖙',
+        footer: 'karma',
         interactiveButtons: playAgainButtons()
     });
 
