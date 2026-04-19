ad1225563ae7da35e56f35eb7882bc24b7af6149
karma <275169041+pastsimpledev@users.noreply.github.com>
2026-04-19 11:55:50 +0200

    Update info-staff.js

--- plugins/info/info-staff.js
+++ plugins/info/info-staff.js
@@ -1,58 +1,31 @@
-//plugin by Giuse
 let handler = async (m, { conn }) => {
+let text = `
+╭━━━━━━━━━━━╮
+┃   👑 *OWNER* 👑   
+╰━━━━━━━━━━━╯
 
-    // Newsletter globale karma
-    const cuContext = {
-        isForwarded: true,
-        forwardingScore: 999,
-        forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363259442839354@newsletter',
-            serverMessageId: 100,
-            newsletterName: `karma ✦ Staff Ufficiale`
-        }
-    };
+✨ *Contatti ufficiali*
 
-    // Schede di contatto (vCard)
-    const vcards = [
-        { vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;Vale | CEO;;;\nFN:Vale | CEO\nORG:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nTITLE:CEO\nitem1.TEL;waid=393773842461:+39 377 384 2461\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
-        { vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;Diego;;;\nFN:Diego\nORG:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nTITLE:Staff\nitem1.TEL;waid=393520583119:+39 352 058 3119\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
-        { vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;Matte;;;\nFN:Matte\nORG:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nTITLE:Staff\nitem1.TEL;waid=66621409462:+66 62 140 9462\nitem1.X-ABLabel:Cellulare\nEND:VCARD` },
-        { vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;Giuse;;;\nFN:Giuse\nORG:𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲\nTITLE:Staff\nitem1.TEL;waid=393291944932:+39 329 194 4932\nitem1.X-ABLabel:Cellulare\nEND:VCARD` }
-    ];
+📞 *WhatsApp*
+wa.me/19782540529
 
-    // Testo elegante con i numeri in chiaro
-    let testo = `
-୧・︶ ✦ karma ✦ ︶・୨
-꒷꒦ ‧₊ 🛡️ 𝐒 𝐓 𝐀 𝐅 𝐅 🛡️ ₊‧ ꒷꒦
-୧・︶ : ︶ : ︶ : ︶ : ︶ : ︶・୨
+📸 *Instagram*
+@identityspoofing
 
-✦ 👑 +39 377 384 2461 ~ Vale |CEO|
-✦ 👨‍💻 +39 352 058 3119 ~ Diego
-✦ 👨‍💻 +66 62 140 9462 ~ Matte
-✦ 👨‍💻 +39 329 194 4932 ~ Giuse
+📧 *Email*
+identityspoofing@gmail.com
 
-👑 _Il team dietro 𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲._
-୧・︶ : ︶ ꒷꒦ ‧₊ ୧`.trim();
+━━━━━━━━━━━━━━━━━━━━
+😈 *FEAR DOMINA*
+⚡ Potere. Stile. Controllo.
+━━━━━━━━━━━━━━━━━━━━
+`
 
-    // 1. Invia le schede contatto (rubrica)
-    await conn.sendMessage(m.chat, {
-        contacts: {
-            displayName: 'Staff 𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲',
-            contacts: vcards
-        },
-        contextInfo: cuContext
-    }, { quoted: m });
+await conn.sendMessage(m.chat, { text }, { quoted: m })
+}
 
-    // 2. Invia il testo stilizzato
-    await conn.sendMessage(m.chat, {
-        text: testo,
-        contextInfo: cuContext
-    });
+handler.help = ['owner']
+handler.tags = ['info']
+handler.command = ['owner']
 
-};
-
-handler.help = ['staff', 'owner', 'creatori'];
-handler.tags = ['info'];
-handler.command = /^(staff|owner|creatori|founder)$/i;
-
-export default handler;
+export default handler
